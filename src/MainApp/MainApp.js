2809fb2a09b3ec3c937e1bd199ef16ffce063496
Zal Machado <zalmachado@gmail.com>
2022-04-16 17:34:46 -0400

    initial files for main app

---
+++ src/MainApp/MainApp.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './MainApp.module.scss';
+
+const MainApp = () => {
+  return (
+    <div className={styles.mainApp}>hello</div>
+  );
+};
+
+export default MainApp;
