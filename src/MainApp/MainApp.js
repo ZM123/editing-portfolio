1216ee762afebd65f52532c39e9593af8a85f64d
Zal Machado <zalmachado@gmail.com>
2022-04-16 17:53:18 -0400

    Add routing

--- src/MainApp/MainApp.js
+++ src/MainApp/MainApp.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Routes, Route } from "react-router-dom";
 import styles from './MainApp.module.scss';
 
 const MainApp = () => {
   return (
-    <div className={styles.mainApp}>hello</div>
+    <div className={styles.mainApp}>
+      <div>Header</div>
+      <Routes>
+        <Route path="/about" element={<div>About</div>} />
+        <Route path="/work" element={<div>Work</div>} />
+        <Route path="/contact" element={<div>Contact</div>} />
+        <Route path="/" element={<div>Home</div>} />
+      </Routes>
+    </div>
   );
 };
 
