057f68842b5aa2c8cd240ef9c87db7a2eaa2d4f4
Zal Machado <zalmachado@gmail.com>
2022-04-19 18:04:58 -0400

    add vfx reel to work page

--- src/pages/Work/Work.js
+++ src/pages/Work/Work.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import VFXThumbnail from '../../assets/VFXReel_Thumbnail.png';
 import PageTemplate from '../PageTemplate';
 import styles from './styles.module.scss';
 
@@ -16,7 +17,12 @@ const Work = () => {
   return (
     <PageTemplate>
       <div className={styles.workPage}>
-        <div className={styles.workItemOne} />
+        <WorkLink
+          href="https://www.youtube.com/watch?v=WBVafHBFtkg"
+          src={VFXThumbnail}
+          title="VFX Reel in Adobe After Effects"
+          description="A video with samples of effects I have made in Adobe After Effects"
+        />
         <WorkLink
           href="https://github.com/ZM123/avid-rename-utility"
           src="https://user-images.githubusercontent.com/3130987/153670574-330ca7f8-f11c-4291-831d-ffb10ac435d3.png"
